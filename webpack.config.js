2adb42c757a27a46a5ec2d02912bd1af0897acf9
Costas Ioannou <ioannou.dev@gmail.com>
2021-10-17 08:32:26 +0300

    install and configure webpack basics

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: [/node_modules/, /coverage/],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'production',
+};
